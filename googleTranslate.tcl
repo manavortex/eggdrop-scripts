6a1d5067767a5b47d876f65d24800bf5cfc115b4
manavortex <manavortex@gmail.com>
2017-09-21 11:19:04 +0200

    Update googleTranslate.tcl
    formatting, comments

--- googleTranslate.tcl
+++ googleTranslate.tcl
@@ -13,47 +13,46 @@ namespace eval googleTranslate {
 
     bind pub - !tr googleTranslate::translate
   
-  proc translate { nick uhost handle chan text } {
+    proc translate { nick uhost handle chan text } {
 
-    set appUrl "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto"
-    
-    package require http 2.7
-    package require tls 1.6
-    package require json
+        set appUrl "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto"
+        
+        package require http 2.7
+        package require tls 1.6
+        package require json
 
+        #set default language 
+        set targetlang "en"
 
-    #set default language 
-    set targetlang "en"
-
-    #try to overwrite with language from translation string:
+        #try to overwrite with language from translation string:
         set lngEx [regexp -all -inline -- {^\s*\w\w\s} $text]
         
-        # if we set a destination language, strip them from text
 
-        # it is not empty
+        # it is not empty: strip it from the translation string and trim both
         if {[binary scan $lngEx c c]} {                    
             regsub -all {\W} $lngEx "" targetlang
-      regsub -all {^\s*\w\w\s} $text "" text
-      set translateme [string trim $text]
-        
-        # it is empty
+            regsub -all {^\s*\w\w\s} $text "" text
+            set translateme [string trim $text]
         } else {
-      set translateme $text
+            set translateme $text
         }
 
+        # encode it as query and generate the URL
+        set translateme [::http::formatQuery  [split $translateme] ]
 
-    set translateme [::http::formatQuery  [split $translateme] ]
-    set appUrl "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto"
-    set translationurl "$appUrl&tl=$targetlang&dt=t&q=$translateme"
+        # use the app url that the chrome translate expansion uses. Please don't abuse this.
+        set appUrl "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto"
+        set translationurl "$appUrl&tl=$targetlang&dt=t&q=$translateme"
 
-    
-     set apioutput [::http::geturl $translationurl -binary 1]     
-    set result [::json::json2dict [encoding convertfrom utf-8 [http::data $apioutput]]]    
-    # three levels deep inside the nested array...
-    set translationtext [lindex $result 0 0 0]
-    
-    putserv "PRIVMSG $chan :$translationtext"
-  }
+        # do encoding magic, thanks SergioR on #egghelp
+        set apioutput [::http::geturl $translationurl -binary 1]     
+        set result [::json::json2dict [encoding convertfrom utf-8 [http::data $apioutput]]]    
+        
+        # three levels deep down in the nested array lieth the truth...
+        set translationresult [lindex $result 0 0 0]
+        
+        putserv "PRIVMSG $chan :$translationresult"
+    }
 
 }
 
